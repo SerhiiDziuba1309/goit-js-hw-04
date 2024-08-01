eb2d0dbc30128e7b1f740d61b449e7a978b02a4f
SerhiiDziuba1309 <168010375+SerhiiDziuba1309@users.noreply.github.com>
2024-08-01 10:32:25 +0200

    add task-2

--- js/task-2.js
+++ js/task-2.js
@@ -3,6 +3,17 @@
 // Кожен об’єкт описує день тижня та кількість калорій calories, спожитих спортсменом, у цей день.
 // Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.У консоль будуть виведені результати її викликів.
 
+function calcAverageCalories(days) {
+  let caloriesEveryday = 0;
+  for (let i = 0; i < days.length; i++) {
+    caloriesEveryday += days[i].calories;
+  }
+  if (days.length === 0) {
+    return 0;
+  }
+  return caloriesEveryday / days.length;
+}
+
 console.log(
   calcAverageCalories([
     { day: 'monday', calories: 3010 },
